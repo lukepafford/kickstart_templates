51c87ffc18e64f6f6d5a707ad1021890918c7d2b
Luke Pafford <lukepafford@gmail.com>
2020-04-20 15:40:50 -0700

    Changed 'creds' to 'data' to reflect logical meaning of the file

--- ks.py
+++ ks.py
@@ -4,23 +4,25 @@ import json, logging, os, sys
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def ks():
-	context = dict()
-	creds = read_creds()
-	context.update(**creds)
-	hostname = request.args.get('hostname')
+    context = dict()
+    data = read_data()
+    context.update(**data)
+    hostname = request.args.get("hostname")
 
-	if not hostname:
-		abort(400, '"hostname" must be set in query arguments')
-	else:
-		context['hostname'] = hostname
-		response = make_response(render_template('ks.cfg', **context))
-		response.mimetype = 'text/plain'
-		return response
+    if not hostname:
+        abort(400, '"hostname" must be set in query arguments')
+    else:
+        context["hostname"] = hostname
+        response = make_response(render_template("ks.cfg", **context))
+        response.mimetype = "text/plain"
+        return response
 
-def read_creds():
-	creds_file = os.path.join(os.getcwd(), 'data.json')
-	with open(creds_file) as f:
-		creds = json.load(f)
-	return creds
+
+def read_data():
+    data_file = os.path.join(os.getcwd(), "data.json")
+    with open(data_file) as f:
+        data = json.load(f)
+    return data
